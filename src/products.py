6a601d4975c51f9e960d797ccac2b822ca427339
Artem Zhitkov <aptem_deadly@mail.ru>
2024-10-12 22:35:13 +0500

    update

--- src/products.py
+++ src/products.py
@@ -1,18 +1,3 @@
-class Product:
-    """Класс описывает название продукта, назначение, цену и количество продукта"""
-
-    name: str
-    description: str
-    price: float
-    quantity: int
-
-    def __init__(self, name, description, price, quantity):
-        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
-
 
 class Category:
     """Класс описывает название категории и её описание. Подсчитывает количество
@@ -28,6 +13,52 @@ class Category:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products if products else []
         Category.product_count += len(products)
         Category.category_count += 1
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        products_string = ""
+        for product in self.__products:
+            products_string += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_string
+
+
+class Product:
+    """Класс описывает название продукта, назначение, цену и количество продукта"""
+
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product: dict):
+        return cls(**product)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        elif new_price < self.__price:
+            confirmation = input(f"Подтвердите понижение цены c {self.__price} до {new_price}: y(да)/n(нет)\n").lower()
+            if confirmation == "y":
+                self.__price = new_price
+        else:
+            self.__price = new_price
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,34 @@
+import json
 from unittest.mock import MagicMock, mock_open, patch
 
 from src.utils import create_objects_from_json, read_json
 
-# @patch("src.utils.json.load")
-# def test_read_json(mock: MagicMock, products_test: list[dict]) -> None:
-#     mock.return_value = products_test
-#     assert read_json('../data/products.json') == products_test
+
+@patch(
+    "builtins.open",
+    new_callable=mock_open,
+    read_data='[{"name": "Смартфоны", "description": "Смартфоны, как средство не только '
+    'коммуникации", "products": [{"name": "Samsung Galaxy C23 Ultra", '
+    '"description": "256GB, Серый цвет, 200MP камера", "price": 180000.0, '
+    '"quantity": 5}, {"name": "Iphone 15", "description": "512GB, Gray space", '
+    '"price": 210000.0, "quantity": 8}]}]',
+)
+def test_read_json(mock_file, products_test) -> None:
+    assert read_json(products_test) == [
+        {
+            "description": "Смартфоны, как средство не только коммуникации",
+            "name": "Смартфоны",
+            "products": [
+                {
+                    "description": "256GB, Серый цвет, 200MP камера",
+                    "name": "Samsung Galaxy C23 Ultra",
+                    "price": 180000.0,
+                    "quantity": 5,
+                },
+                {"description": "512GB, Gray space", "name": "Iphone 15", "price": 210000.0, "quantity": 8},
+            ],
+        }
+    ]
 
 
 @patch("builtins.open", new_callable=mock_open, read_data="")
@@ -19,6 +42,7 @@ def test_file_not_found(mock_file: MagicMock) -> None:
     assert read_json("../data/products.json") == [{}]
 
 
-def test_create_objects_from_json(products_test: list[dict]) -> None:
-    result = create_objects_from_json(products_test)
+def test_create_objects_from_json(products_test) -> None:
+    data = json.loads(products_test)
+    result = create_objects_from_json(data)
     assert result[0].name == "Смартфоны"
